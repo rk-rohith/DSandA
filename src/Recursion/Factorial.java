a9e1a10ec96b9104d74d81d49e4a21904b61a974
RK00618846 <Madmax@!589>
2022-05-18 09:57:11 +0530

    Factorial

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -1,16 +1,20 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
-        int n = 5;
-        if(n>=0){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = scanner.nextInt();
+        if (n >= 0) {
             int factorial = fact(n);
-            System.out.println("factorial of "+n+" is "+factorial);
+            System.out.println("factorial of " + n + " is " + factorial);
         }
     }
 
     private static int fact(int n) {
-        if(n==0 || n==1) return 1;
-        return n*fact(n-1);
+        if (n == 0 || n == 1) return 1;
+        return n * fact(n - 1);
     }
 }
