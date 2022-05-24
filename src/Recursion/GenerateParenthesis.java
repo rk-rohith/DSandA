8786e45247bd30db5738d9bd43b101568c799252
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2022-05-24 10:26:53 +0530

    Update GenerateParenthesis.java

--- src/Recursion/GenerateParenthesis.java
+++ src/Recursion/GenerateParenthesis.java
@@ -1,8 +1,9 @@
-package Recursion; /**
+/**
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  * Input: n = 3
  * Output: ["((()))","(()())","(())()","()(())","()()()"]
  */
+package Recursion; 
 
 import java.util.ArrayList;
 import java.util.List;
