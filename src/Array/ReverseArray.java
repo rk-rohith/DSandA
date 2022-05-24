a434d30769b85c7faa190d0ed8164c3cf5d47190
Rohith Kumar <88598718+rk-rohith@users.noreply.github.com>
2022-05-24 18:09:51 +0530

    reverse array

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,25 @@
+/***
+ * Reverse an Array
+ */
+package Recursion;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        int[] revArray = arrayReverse(arr);
+        System.out.println(Arrays.toString(revArray));
+    }
+
+    private static int[] arrayReverse(int[] arr) {
+        int[] revArray= new int[arr.length];
+        int j = arr.length-1;
+        for (int k : arr) {
+            revArray[j] = k;
+            j = j - 1;
+        }
+        return revArray;
+    }
+}
