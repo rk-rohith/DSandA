e371cc57ccf316a15bbe077c35683cdc5fa1c90e
Rohith Kumar N <rk00618846@techmahindra.com>
2022-08-04 10:53:25 +0530

    Character Occurrence and Remove Duplicate

---
+++ src/String/OccurrenceOfCharacter.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurrenceOfCharacter {
+    public static void main(String[] args) {
+        String str = "RohtangLa";
+        char[] ch = str.toLowerCase().toCharArray();
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char c : ch) {
+            if (!charMap.containsKey(c)) {
+                charMap.put(c, 1);
+            } else {
+                charMap.put(c, charMap.get(c) + 1);
+            }
+        }
+        System.out.println(charMap);
+
+        /* Remove Duplicates*/
+        for (Map.Entry<Character, Integer> entries : charMap.entrySet()) {
+            if (entries.getValue() == 1) {
+                System.out.println(entries.getKey());
+            }
+        }
+
+    }
+}
