2d05a5bceff6c9a1ab5f838ab64469ac7c195c05
Rohith Kumar N <rk.rohithkumar97@gmail.com>
2022-08-28 18:12:47 +0530

    problem statement for Kth smallest term

--- src/Array/KthSmallestTerm.java
+++ src/Array/KthSmallestTerm.java
@@ -1,5 +1,19 @@
 package Array;
 
+/**
+ * For an array arr of n elements, the cross product of the array, cp_arr, is defined as cp_arr = {(arr[i], arr[i])} for all 0<=i, j<n.
+ *
+ * Example 1:
+ * There are n=3 elements in arr=[3,1,2] and K=7.
+ * The cross product array, cp_arr = [(3, 3), (3, 1), (3, 2), (1, 3), (1, 1), (1, 2), (2, 3), (2, 1), (2, 2)).
+ * When sorted, cp_arr = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]. The kth element, element 7 is (3, 1)
+ *
+ * Example 2:
+ * There are n=2 elements in arr=[4,1] and K=3.
+ * cp_arr = [(4,4),(4,1),(1,4), (1, 1)]
+ * Sorted, cp_arr = [(1, 1), (1,4),(4,1),(4.4)]. The 3rd element is (4,1).
+ */
+
 import java.util.*;
 
 public class KthSmallestTerm {
