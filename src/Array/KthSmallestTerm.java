fc1856abfd5904ac46aebf4cdbb9421a7fa52ee5
Rohith Kumar <rk00618846@techmahindra.com>
2022-08-28 16:42:33 +0530

    Kth smallest term

---
+++ src/Array/KthSmallestTerm.java
@@ -0,0 +1,37 @@
+package Array;
+
+import java.util.*;
+
+public class KthSmallestTerm {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter number of elements");
+        Integer n = s.nextInt();
+        Integer[] arr = new Integer[n];
+        System.out.println("enter elements");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        System.out.println("enter the Kth smallest term");
+        Integer k = s.nextInt();
+
+        System.out.println("enter the Kth smallest term is " + findKthSmaalestTerm(arr, k));
+
+    }
+
+    private static List<Integer> findKthSmaalestTerm(Integer[] arr, Integer k) {
+        List<Integer> integers = Arrays.asList(arr);
+        Collections.sort(integers);
+
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < integers.size(); i++) {
+            for (int j = 0; j < integers.size(); j++) {
+                list.add(Arrays.asList(integers.get(i), integers.get(j)));
+            }
+        }
+        System.out.println(list);
+        return list.get(k);
+    }
+}
